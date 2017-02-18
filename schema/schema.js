34a88a1d7fdeeba70aaa402be04adfbb821cab93
Jasen Pan <jasenpan@192-168-1-100.tpgi.com.au>
2017-02-18 15:55:25 +1100

    first commit

---
+++ schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt
+} = graphql;
+
+const users = [
+    { id: "23", firstName: "Foo", age: 10 },
+    { id: "44", firstName: "Bar", age: 20 },
+    { id: "51", firstName: "Baz", age: 30 }
+];
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args){
+                return _.find(users, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use("/graphql", expressGraphQL({
+    schema,
+    graphiql: true
+}));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log("Server listen to port " + PORT);
+});
